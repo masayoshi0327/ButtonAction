105b62b993b75012df73b0f3523308dd019a89fd
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-03-07 23:35:02 +0900

    Initial Commit

---
+++ ButtonAction/ButtonActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonActionApp.swift
+//  ButtonAction
+//
+//  Created by 澤崎正佳 on 2021/03/07.
+//
+
+import SwiftUI
+
+@main
+struct ButtonActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
